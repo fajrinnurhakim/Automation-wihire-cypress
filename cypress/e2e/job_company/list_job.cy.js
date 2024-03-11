3b7dd0d9b4498c51029edbd57f416da1d3c20146
Fajrin Nurhakim <fajrinnurhakim17@gmail.com>
2024-03-11 16:11:09 +0700

    add: test job company list

---
+++ cypress/e2e/job_company/list_job.cy.js
@@ -0,0 +1,19 @@
+import list_job from "../../support/page_object/job_company/list_job";
+
+describe("Wihire Website", () => {
+    beforeEach(() => {
+        cy.visit(Cypress.env("BASE_URL"));
+    });
+
+    it("Display list category", () => {
+        list_job.listCategory();
+    });
+
+    it("Display list search", () => {
+        list_job.listSearch();
+    });
+
+    it("Display list dropdown", () => {
+        list_job.listDropdown();
+    });
+});
